39c3cc3fc4fd9cea6f976be6a0f82c34b2c81a0a
Tomek <tomek.bielicki@gmail.com>
2022-05-10 20:49:39 +0200

    init commit, connect to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=53290998ea334cafbf1183417221005&q=London&aqi=no"
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  return <div className="App"></div>;
 }
 
 export default App;
