ab26d146cd6fd375d58ecee6bb8488f10e57246b
Tomek <tomek.bielicki@gmail.com>
2022-05-13 17:19:25 +0200

    header style added

--- src/components/ForecastHeader/ForecastHeader.styled.js
+++ src/components/ForecastHeader/ForecastHeader.styled.js
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
-export const HeaderStyle = styled.header``;
+export const HeaderStyle = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: green;
+  justify-content: space-between;
+  margin-bottom: 200px;
+  height: 50px;
+  width: auto;
+`;
 
-export const HeaderTile = styled.div``;
+export const HeaderTile = styled.div`
+  color: white;
+  font-weight: bold;
+  font-size: 30px;
+  margin: 5px;
+`;
