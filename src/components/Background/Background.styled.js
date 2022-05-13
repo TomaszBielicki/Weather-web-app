bebbb7ec5eb063e468e6d64d5f592065824dbe5f
Tomek <tomek.bielicki@gmail.com>
2022-05-13 17:18:34 +0200

    background styled

---
+++ src/components/Background/Background.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  margin: 0px;
+  padding: 0px;
+  height: auto;
+  background-image: url("lake-background.jpeg");
+`;
+
+// no-repeat 50% 40% / cover
