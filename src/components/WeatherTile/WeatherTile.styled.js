4365e9edec391b15d122960c19b8a88490969ee6
Tomek <tomek.bielicki@gmail.com>
2022-05-13 17:19:10 +0200

    weather tile default styled

--- src/components/WeatherTile/WeatherTile.styled.js
+++ src/components/WeatherTile/WeatherTile.styled.js
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
-export const TileWeather = styled.div``;
+export const TileWeather = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding-left: 10px;
+  margin: 5px;
+  background-color: grey;
+  width: 300px;
+  height: 100px;
+  font-size: 30px;
+  color: white;
+  font-family: sans-serif;
+  border-radius: 10px;
+`;
+
+export const TileList = styled.div`
+  width: auto;
+  margin-top: 200px;
+  display: flex;
+  padding-left: 10px;
+  padding-right: 10px;
+  justify-content: space-between;
+  background-color: black;
+`;
