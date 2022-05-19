bc3c967c23cacd5edf8fe101407cc8b896cb5810
Tomek <tomek.bielicki@gmail.com>
2022-05-19 18:12:25 +0200

    line comp created

---
+++ src/Line/Line.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { OneLine } from "./Line.styled";
+
+export default function Line() {
+  return <OneLine></OneLine>;
+}
---
+++ src/Line/Line.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const OneLine = styled.div`
+  height: 5px;
+  width: auto;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  background-color: black;
+`;
